9adf7ae5ad643f8aeddad1de5d3cd03f97a0f9bc
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-09 10:32:37 -0500

    Create Alexandre.py

---
+++ Alexandre.py
@@ -0,0 +1,87 @@
+Problème avec les classes, doit relire les notes de cours et lire plus encore
+def lire_donnees(path : str):
+
+    with open(path, "r", encoding="UTF-8") as f:
+        numero = ""
+        mdp = []
+        donnees = f.readlines()
+
+class Utilisateur:
+    def __init__(self, numero: str, mdp: int):
+        self.numero = numero
+        self.mdp = mdp
+
+print('Entrer votre numéro de compte et votre mot de passe')
+    if self.numero = test (premiere ligne) and self.mdp = test (premiere ligne)
+        print(menu1)
+    else 
+        print(Numero ou mdp erroné)
+
+def menu1():
+    menu1 = {1: 'Chèque',
+             2: 'Épargne',
+             3: 'Placement'}
+print()
+for num, opt in menu.items(): #(MENU ITEM = TEXTOUI OUI)
+    print(f"{num} - {opt}")
+
+        choix = int(input("Entrer votre choix: "))
+        print() 
+
+if cheque 
+    print menu2
+
+def menu2():
+    paquet = initialisation()
+    sortie = False
+    while not sortie:
+        menu2 = {1: 'Faire un dépot',
+                 2: 'Faire un retrait',
+                 3: 'Terminer la transaction'}
+
+choix = int(input("Entrer votre choix: "))
+print() 
+
+if choix == 1:
+    print(Dépot)
+    self.nom +1 
+
+elif choix == 2:
+    print(Retrait)
+
+elif choix == 3:
+    print(Terminer)
+
+if epargne 
+print menu2
+#MEME JE PEUX FAIRE PRINT MENU 1 POUR LES DEUX PREMIERS CHOIX
+if choix == 1:
+    print(Dépot)
+
+elif choix == 2:
+    print(Retrait)
+
+elif choix == 3:
+    print(Terminer)
+
+#doit faire dépot + retrait et terminer PUIS crypter dans le doc les mdp et nom
+##Je dois enregistrer les doc puis créer un double pour inscire l'incryption
+#classe argent compte 2 et compte 3??
+
+placement 
+compte 1
+print argent 
+print taux intéret
+print retour
+
+  sortie = False
+Csolde = Cinitial (1 + intérêt )nombre d'années
+an 1 , ans 2, ans 3, ans 4, ans 5
+
+menu4 = 
+Ajouter un compteÉcrire dans doc numero + mdp
+Enlever compte Effacer dans doc numero + mdp
+print le doc puis dire quel numéro effacer (1, 2, 3)
+canegr taux interet 
+exit = stop 
+
